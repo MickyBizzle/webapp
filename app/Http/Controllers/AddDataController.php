9fca4427a42028e00ae3ea0bb7845c074ffc30c4
Michael Ballard <mib26@svmib26.dcs.aber.ac.uk>
2018-02-23 17:03:14 +0000

    setup working

---
+++ app/Http/Controllers/AddDataController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class AddDataController extends controller
+{
+
+  public function add() {
+    return ("IT WORKS!");
+  }
+
+}
